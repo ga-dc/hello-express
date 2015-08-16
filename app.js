6932624d7c9f797737917db261c29c7ad7efc30d
Andy <andrew.sunglae.kim@gmail.com>
2015-08-16 19:58:35 -0400

    add solution

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express")
 var app = express()
 
+// configure app to use body parser
+var bodyParser = require("body-parser")
+
+app.use(bodyParser.json()) //handles json post requests
+app.use(bodyParser.urlencoded({ extended: true })) // handles form submissions
 // sets view engine to hbs
 app.set("view engine", "hbs")
 
@@ -13,6 +18,10 @@ app.get("/:name",function (req,res){
   res.render("hello", {
     name: req.params.name
   })
+})
+
+app.post("/", function(req, res){
+  res.render("hello", {name: req.body.name})
 })
 
 // which port number to listen for
